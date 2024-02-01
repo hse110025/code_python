cf7902d3bb8c881c1a4b4e455bf4a855a1dc2844
hse110025 <hse110025@gmail.com>
2024-02-01 22:14:50 +0900

    다이다

--- solved_ac/solved_ac_2508.py
+++ solved_ac/solved_ac_2508.py
@@ -1,10 +1,24 @@
 num = int(input(""))
 num2 = input("")
 row,col=input().split(" ")
+row=int(row)
+col=int(col)
 arr=[]
-for i in range(int(row)):
+count = 0
+for i in range(row):
   k=list(input(""))
   arr.append(k)
 
 print(arr)
-  
+for i in range(row):
+  if 'o' not in arr[i]:
+    continue
+  else:
+    a=arr[i].index("o")
+    if i!=0 and i!=row-1:
+      if arr[i-1][a]=='v' and arr[i+1][a]=='^':
+        count+=1
+    if a!=0 or a!=col-1:
+      if arr[i][a-1]=='>' and arr[i][a+1]=='<':
+        count+=1
+print(count)    
