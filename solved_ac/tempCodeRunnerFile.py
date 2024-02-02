379b029b0256b97e6bf7ed0c2d6bbeb175812f2b
hse110025 <hse110025@gmail.com>
2024-02-02 13:17:14 +0900

    2024_02_02_update1

--- solved_ac/tempCodeRunnerFile.py
+++ solved_ac/tempCodeRunnerFile.py
@@ -16,7 +16,7 @@ for kkp in range(num):
       continue
     else:
       for _ in range(arr[i].count("o")):
-        a=arr[i][k+1:].index('o')+1+k
+        a=arr[i][k+1:].index("o")+1+k
         k=a
         if i!=0 and i!=row-1:
           if arr[i-1][a]=='v' and arr[i+1][a]=='^':
